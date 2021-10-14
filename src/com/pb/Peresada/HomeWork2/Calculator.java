95a6baf151672bc301301d1894e1a3a446df5f06
Alexandr Peresada <Aleksandr.Peresada@privatbank.ua>
2021-10-14 10:22:44 +0300

    Done

---
+++ src/com/pb/Peresada/HomeWork2/Calculator.java
@@ -0,0 +1,40 @@
+package com.pb.Peresada.HomeWork2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int operand1 = 'x';
+        int operand2 = 'y';
+        String sign = "z";
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите первое число");
+        int x = in.nextInt();
+
+        System.out.println("Введите второе число");
+        int y = in.nextInt();
+
+        System.out.println("Укажите операцию над числами. Доступные операции: '+'  суммирование, '-' вычитание, '*' умножение,'/' деление ");
+        String z = in.next();
+
+        switch (z) {
+            case "+":
+                System.out.println(x + y);
+                break;
+            case "*":
+                System.out.println(x * y);
+                break;
+            case "-":
+                System.out.println(x - y);
+                break;
+            case "/":
+                if (y == 0) {
+                    System.out.print("Нельзя делить на 0!");
+                } else {
+                    System.out.println(x / y);
+                }
+                break;
+        }
+            }
+}
---
+++ src/com/pb/Peresada/HomeWork2/Interval.java
@@ -0,0 +1,15 @@
+package com.pb.Peresada.HomeWork2;
+
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+        int operand = 'x';
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int x = in.nextInt();
+
+
+    }
+
+}
